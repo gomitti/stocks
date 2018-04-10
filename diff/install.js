e83dd2346ec1a43f73d1d5768972fb053492e6e3
belugafm <13643825+belugafm@users.noreply.github.com>
2018-04-11 01:04:59 +0900

    :whale:

--- diff/install.js
+++ diff/install.js
@@ -3,7 +3,11 @@ const { MongoClient } = require("mongodb")
 const db_url = "mongodb://localhost:27017"
 const db_name = "diff"
 
-MongoClient.connect(db_url, function (err, client) {
+MongoClient.connect(db_url, function (error, client) {
+    if(error){
+        console.log(error)
+        return
+    }
     const db = client.db(db_name)
     db.collection("stocks").createIndex({ "code": -1, "url": -1 }, { "unique": true })
     client.close()
