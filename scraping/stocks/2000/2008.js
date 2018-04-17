a8f889101c817b6bb9e819a69e08f38676f58c98
hrszz <g0m1tt1@outlook.jp>
2018-04-17 02:17:39 +0000

    🤔 done 2009. but not implemented 2009

--- scraping/stocks/2000/2008.js
+++ scraping/stocks/2000/2008.js
@@ -6,23 +6,28 @@ const gen = require('../../urlgen').urlGen
 const oldest = 2009
 const formatFunc = year => `http://www.masufun.co.jp/ir/release/${year}/`
 module.exports = {
-  'code': 2008,
-  'name': '増田粉',
-  'parsers': [{
-    'targets': gen(oldest, formatFunc),
-    'format': 'html',
-    'parse': $ => {
-      const result = []
-      $('div.topicsList').each(function (idx, elem) {
-        const $item = $(elem)
-        const title = $item.find('a').text().trim()
-        if (typeof title !== 'string') { throw new Error('タイトルを取得できません') }
-        const url = resolver(samplePath, $item.find('a').attr('href'))
-        const date = Date.parse($item.find('dt').text().trim())
-        if (!(date instanceof Date)) { throw new Error('日付を取得できません') }
-        result.push({ title, url, date })
-      })
-      return result
-    }
-  }]
+    'code': 2008,
+    'name': '増田粉',
+    'parsers': [{
+        'targets': gen(oldest, formatFunc),
+        'format': 'html',
+        'parse': $ => {
+            const result = []
+            $('div.topicsList').each(function(idx, elem) {
+                const $item = $(elem)
+                const title = $item.find('a').text().trim()
+                if (typeof title !== 'string') { throw new Error('タイトルを取得できません') }
+                const url = resolver(samplePath, $item.find('a').attr('href'))
+                const arrDate = $item.find('dt').text().trim().split('.').map(str => parseInt(str, 10))
+                if (arrDate.length !== 3 ||
+                    arrDate.filter(x => !isFinite(x)).length > 0) {
+                    throw new Error('日付を取得できません')
+                }
+                const date = new Date(arrDate[0], (arrDate[1] + 11) % 12, arrDate[2])
+                if (!(date instanceof Date)) { throw new Error('日付を取得できません') }
+                result.push({ title, url, date })
+            })
+            return result
+        }
+    }]
 }
