013c105e18d42861e848a1ced200645de82d12dd
hrszz <g0m1tt1@outlook.jp>
2018-05-09 02:33:05 +0000

    🤔  scraping 2000/2058

--- scraping/stocks/2000/2058.js
+++ scraping/stocks/2000/2058.js
@@ -12,7 +12,6 @@ module.exports = {
             const result = []
             $('div[class=news-content]').each((idx, elem) => {
                 let dateTitleObj = null
-                "「」"
                 dateTitleObj = $($(elem).eq(0))
 
 
@@ -34,16 +33,44 @@ module.exports = {
                 const date = new Date(2000 + arrDate[0], (arrDate[1] + 11) % 12, arrDate[2])
                 if (!(date instanceof Date)) { throw new Error('日付を取得できません') }
 
-                const title = dateTitleObj
+                //"「」"に囲まれたテキストを得る　 
+                //"『』"がある場合はこちらを優先する
+                let title = dateTitleObj
                     .clone() //clone the element
                     .children() //select all the children
                     .remove() //remove all the children
-                    .end() //again go back to selected element
                     .text().trim();
+                title = title.substr(0, title.indexOf("\n"))
+                //末尾の冗長な表現を削除する
+                const endStrArr = [
+                    "を発表しました",
+                    "が発売されました",
+                    "を更新しました",
+                    "を掲載しました",
+                    "を開設しました"
+                ]
+                endStrArr.forEach(function(endStr,index) {
+                    if (title.indexOf(endStr) > 0) {
+                        title = title.substr(0, title.indexOf(endStr))
+                    }
+                })
+                
+                //日付表現も冗長なので削除する
+                if (title.match(/[0-9][0-9].[0-1][0-9].[0-3][0-9]/)) {
+                    title = title.replace(/[0-9][0-9].[0-1][0-9].[0-3][0-9]/, '')
+                }
+                title = title.trim()
+                if (title == void 0) {
+                    throw new Error('タイトルを取得できません')
+                }
+
 
                 const url = $(elem).find('div.MT5 > a').attr('href')
+                // URLがないニュースリリースは現状エラーとしておく
+                if (url === undefined) {
+                    throw new Error(`ニュースリリース「${title}」のリンク先を取得できません`)
+                }
                 const item = { title, url, date }
-                console.log(item)
                 result.push(item)
             })
             return result
